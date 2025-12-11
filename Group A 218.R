07317a0bfb743e84413d23fa114e55ca72c91ea6
gopal425 <gs25abh@herts.ac.uk>
2025-12-11 15:21:07 +0530

    Update Group A 218.R

--- Group A 218.R
+++ Group A 218.R
@@ -2,3 +2,11 @@ library(ggplot2)
 library(dplyr)
 
 spotify <- read.csv("spotify-2023.csv",
+                    stringsAsFactors = FALSE,
+                    fileEncoding = "latin1",
+                    check.names = FALSE)
+
+
+spotify_clean <- spotify %>%
+  filter(mode %in% c("Major", "Minor")) %>%
+  mutate(
